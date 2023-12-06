9a7be4153d12eee951b8f458bf966023434a2235
FLAVIU-PAUL IEPAN <flaviu.iepan@stud.ubbcluj.ro>
2023-12-06 21:00:59 +0200

    #laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,4 +1,4 @@
-using Iepan_Flaviu_Lab7.Models;
+﻿using Iepan_Flaviu_Lab7.Models;
 namespace Iepan_Flaviu_Lab7;
 
 public partial class ListPage : ContentPage
@@ -6,6 +6,7 @@ public partial class ListPage : ContentPage
 	public ListPage()
 	{
 		InitializeComponent();
+
 	}
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
@@ -20,4 +21,22 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+    
+
 }
